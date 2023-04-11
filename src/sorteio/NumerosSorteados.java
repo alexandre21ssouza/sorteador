3912629ee5595e9b2745e0241949a7f86fdb239f
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-04-11 14:20:54 -0300

    Criação da classe Números Sorteados

---
+++ src/sorteio/NumerosSorteados.java
@@ -0,0 +1,14 @@
+package sorteio;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NumerosSorteados {
+	public static void main(String[] args) {
+		System.out.println("Bem-vindo ao Sorteador!");
+	
+
+		
+	}
+}
