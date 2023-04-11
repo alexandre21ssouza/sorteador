207bfa15a9fb38f53d8700fb5212771ae21ac432
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-04-11 18:46:35 -0300

    Adição da Classe Scanner e criação das variáveis

--- src/sorteio/NumerosSorteados.java
+++ src/sorteio/NumerosSorteados.java
@@ -1,11 +1,23 @@
 package sorteio;
 
-
+import java.util.Scanner;
 
 public class NumerosSorteados {
 	public static void main(String[] args) {
 		System.out.println("Bem-vindo ao Sorteador!");
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Digite quantos números serão sorteados: ");
+		int n = sc.nextInt();
+
+		System.out.println("Digite o menor número do sorteio: ");
+		int nMenor = sc.nextInt();
+		
+		System.out.println("Digite o maior número sorteio: ");
+		int nMaior = sc.nextInt();
+
+		
+		sc.close();
 		
 	}
 }
